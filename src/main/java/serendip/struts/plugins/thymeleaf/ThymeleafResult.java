05143b1ed75aa2c1a65654a91a5a90a44ec7aeab
A-pZ <a-pz@h4.dion.ne.jp>
2016-06-05 21:59:35 +0900

    thymeleaf3.0.0 first-commit. (#1)

---
+++ src/main/java/serendip/struts/plugins/thymeleaf/ThymeleafResult.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2013 Steven Benitez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package serendip.struts.plugins.thymeleaf;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.StrutsConstants;
+import org.thymeleaf.TemplateEngine;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.Result;
+import com.opensymphony.xwork2.inject.Inject;
+
+import serendip.struts.plugins.thymeleaf.spi.TemplateEngineProvider;
+
+/**
+ * Renders a Thymeleaf template as the result of invoking a Struts action.
+ *
+ * @author A-pZ ( Original source : Steven Benitez )
+ * @since 2.3.15
+ */
+public class ThymeleafResult implements Result {
+  private String defaultEncoding = "UTF-8";
+  private TemplateEngineProvider templateEngineProvider;
+  private String templateName;
+
+  /**
+   * The result parameter name to set the name of the template to.
+   *
+   * IMPORTANT! Struts2 will look for this field reflectively to determine which
+   * parameter is the default. This allows us to have a simplified result
+   * configuration. Don't remove it!
+   */
+  public static final String DEFAULT_PARAM = "templateName";
+
+  public ThymeleafResult() {
+  }
+
+  public ThymeleafResult(String templateName) {
+    this.templateName = templateName;
+  }
+
+  @Override
+  public void execute(ActionInvocation actionInvocation) throws Exception {
+    TemplateEngine templateEngine = templateEngineProvider.get();
+
+    HttpServletRequest request = ServletActionContext.getRequest();
+    HttpServletResponse response = ServletActionContext.getResponse();
+    ServletContext servletContext = ServletActionContext.getServletContext();
+
+    Object action = actionInvocation.getAction();
+    StrutsContext context = new StrutsContext(request, response, servletContext, action);
+    response.setContentType("text/html");
+    response.setCharacterEncoding(defaultEncoding);
+    templateEngine.process(templateName, context, response.getWriter());
+  }
+
+  @Inject(StrutsConstants.STRUTS_I18N_ENCODING)
+  public void setDefaultEncoding(String defaultEncoding) {
+    this.defaultEncoding = defaultEncoding;
+  }
+
+  @Inject
+  public void setTemplateEngineProvider(TemplateEngineProvider templateEngineProvider) {
+    this.templateEngineProvider = templateEngineProvider;
+  }
+
+  public void setTemplateName(String templateName) {
+    this.templateName = templateName;
+  }
+}
---
+++ src/main/java/serendip/struts/plugins/thymeleaf/diarect/FieldErrorAttributeProcessor.java
@@ -0,0 +1,173 @@
+/**
+ * 
+ */
+package serendip.struts.plugins.thymeleaf.diarect;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.struts2.ServletActionContext;
+import org.thymeleaf.IEngineConfiguration;
+import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.engine.AttributeName;
+import org.thymeleaf.model.IAttribute;
+import org.thymeleaf.model.IProcessableElementTag;
+import org.thymeleaf.processor.element.AbstractAttributeTagProcessor;
+import org.thymeleaf.processor.element.IElementTagStructureHandler;
+import org.thymeleaf.standard.expression.IStandardExpressionParser;
+import org.thymeleaf.standard.expression.StandardExpressions;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.unbescape.html.HtmlEscape;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.util.ValueStack;
+
+/**
+ * Struts2 Field-error attribute processor.
+ * 
+ * This processor output field-error as css-class 'errorclass'.
+ * 
+ * @author A-pZ
+ *
+ */
+public class FieldErrorAttributeProcessor extends AbstractAttributeTagProcessor {
+	
+	public FieldErrorAttributeProcessor(final String dialectPrefix) {
+		super(
+	            TemplateMode.HTML, // This processor will apply only to HTML mode
+	            dialectPrefix,     // Prefix to be applied to name for matching
+	            null,              // No tag name: match any tag name
+	            false,             // No prefix to be applied to tag name
+	            ATTR_NAME,         // Name of the attribute that will be matched
+	            true,              // Apply dialect prefix to attribute name
+	            PRECEDENCE,        // Precedence (inside dialect's own precedence)
+	            true);             // Remove the matched attribute afterwards
+	}
+	
+	private static final String ATTR_NAME = "value";
+    private static final int PRECEDENCE = 1010;
+	
+    
+    
+	/* (非 Javadoc)
+	 * @see org.thymeleaf.processor.element.AbstractAttributeTagProcessor#doProcess(org.thymeleaf.context.ITemplateContext, org.thymeleaf.model.IProcessableElementTag, org.thymeleaf.engine.AttributeName, java.lang.String, org.thymeleaf.processor.element.IElementTagStructureHandler)
+	 */
+	@Override
+	protected void doProcess(ITemplateContext context, IProcessableElementTag tag, AttributeName attributeName,
+			String attributeValue, IElementTagStructureHandler structureHandler) {
+		final IEngineConfiguration configuration = context.getConfiguration();
+
+        /*
+         * Obtain the Thymeleaf Standard Expression parser
+         */
+        final IStandardExpressionParser parser =
+                StandardExpressions.getExpressionParser(configuration);
+
+        /*
+         * Parse the attribute value as a Thymeleaf Standard Expression
+         */
+        //final IStandardExpression expression = parser.parseExpression(context, attributeValue);
+        
+        // bind class attribute value for field-error.
+        String fieldname = tag.getAttributeValue("name");
+        if ( !hasFieldError(fieldname)) {
+        	return;
+        }
+        
+        // bind request parameter
+        String parameterValue = getFieldValue(fieldname);
+        structureHandler.setAttribute("value", HtmlEscape.escapeHtml5(parameterValue));
+        
+        // add field-error css class.
+        IAttribute cssClass = tag.getAttribute("class");
+        String css = cssClass.getValue();
+        if ( StringUtils.isBlank(css)) {
+        	structureHandler.setAttribute("class", fieldErrorClass(tag));
+        } else {
+        	structureHandler.setAttribute("class", fieldErrorClass(tag) + " " + css);
+        }
+    }
+	
+	/**
+	 * If Struts2 has field-error for request parameter name , return true. 
+	 * @param fieldname request-parameter name
+	 * @return if field-error has target field name, return true.
+	 */
+	protected boolean hasFieldError(String fieldname) {
+		if ( StringUtils.isEmpty(fieldname)) {
+			return false;
+		}
+		
+		Object action = ActionContext.getContext().getActionInvocation().getAction();
+		// check action instance 'ActionSupport'.
+		if (!(action instanceof ActionSupport)) {
+			return false;
+		}
+
+		ActionSupport asupport = (ActionSupport) action;
+		Map<String, List<String>> fieldErrors = asupport.getFieldErrors();
+		if (fieldErrors == null || fieldErrors.size() == 0) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Return Strus2 field value.
+	 * @param fieldname fieldname
+	 * @return fieldvalue
+	 */
+	protected String getFieldValue(String fieldname) {
+		String[] value = (String[])ActionContext.getContext().getParameters().get(fieldname);
+		
+		String overwriteValue = getOverwriteValue(fieldname);
+		
+		if ( overwriteValue != null ) {
+			return overwriteValue;
+		}
+		return value[0];
+	}
+	
+	protected String fieldErrorClass(IProcessableElementTag tag) {
+		if ( tag.getAttribute("error-css") == null) {
+			return "field-error";
+		}
+		
+		String css = tag.getAttribute("error-css").getValue();
+		return css;
+	}
+	/**
+	 * If Type-Convertion Error found at Struts2, overwrite request-
+	 * parameter same name.
+	 *
+	 * @param fieldname parameter-name
+	 * @return request-parameter-value(if convertion error occurs,return from struts2 , not else thymeleaf.)
+	 */
+	protected String getOverwriteValue(String fieldname) {
+		ActionContext ctx = ServletActionContext.getContext();
+		ValueStack stack = ctx.getValueStack();
+		Map<Object ,Object> overrideMap = stack.getExprOverrides();
+		
+		
+		
+		if ( overrideMap == null || overrideMap.isEmpty()) {
+			return null;
+		}
+
+		if (! overrideMap.containsKey(fieldname)) {
+			return null;
+		}
+
+		String convertionValue = (String)overrideMap.get(fieldname);
+
+		// Struts2-Conponent is wrapped String quote, which erase for output value.
+		String altString =  StringEscapeUtils.unescapeJava(convertionValue);
+		altString = altString.substring(1, altString.length() -1);
+
+		return altString;
+	}
+}
