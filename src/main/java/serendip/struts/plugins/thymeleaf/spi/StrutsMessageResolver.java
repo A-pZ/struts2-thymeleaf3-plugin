e4d081657c6dd836a6934ffc4996bc2e8dfbd90f
A-pZ <g.alpha.pz@gmail.com>
2018-09-17 01:31:00 +0900

    Fix MessageResolvers #29
    @psyuhen fork version 1.0.5.1, this work fine.

--- src/main/java/serendip/struts/plugins/thymeleaf/spi/StrutsMessageResolver.java
+++ src/main/java/serendip/struts/plugins/thymeleaf/spi/StrutsMessageResolver.java
@@ -1,28 +1,18 @@
 package serendip.struts.plugins.thymeleaf.spi;
 
-import org.thymeleaf.context.ITemplateContext;
-import org.thymeleaf.messageresolver.AbstractMessageResolver;
-
-import com.opensymphony.xwork2.ActionContext;
-import com.opensymphony.xwork2.util.GlobalLocalizedTextProvider;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.spring4.messageresolver.SpringMessageResolver;
 
 /**
- * For struts2 messages bridge to Thymeleaf3.
+ * For struts2 messages bridge to Thymeleaf3. Use SpringMVV Message Source.
  * @author A-pZ
  *
  */
-public class StrutsMessageResolver extends AbstractMessageResolver {
+@Component
+public class StrutsMessageResolver extends SpringMessageResolver {
 
-	@Override
-	public String resolveMessage(ITemplateContext context, Class<?> origin, String key, Object[] messageParameters) {
-		GlobalLocalizedTextProvider textProvider = new GlobalLocalizedTextProvider();
-		return textProvider.findDefaultText(key, ActionContext.getContext().getLocale(), messageParameters);
-	}
-
-	@Override
-	public String createAbsentMessageRepresentation(ITemplateContext context, Class<?> origin, String key,
-			Object[] messageParameters) {
-		return "??" + key + "??";
+	public StrutsMessageResolver() {
+		setMessageSource(new StrutsMessageSource());
 	}
 
 }
